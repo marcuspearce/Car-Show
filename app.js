69582c0691a2c0108e022e242daf63dc2361d6d1
Marcus Pearce <marcuspearce7@gmail.com>
2018-08-13 19:02:47 +0000

    add Restful Routes to be implemented, bodyParser and mongoose for later

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 // CARSHOW
 
-var express = require("express");
-var app = express();
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+    
+// Connect to Database
+//mongoose.connect("mongodb://localhost:27017/carShow", {useNewUrlParser:true});
 
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({encoded: true})); //copy-paste this line
+app.set("view engine", "ejs"); //expect ejs in rendering
 
 
-// ROOT ROUTE
+// ROUTE ROUTE
 app.get('/',function(req,res){
     res.render("home");
+});
+
+
+// INDEX ROUTE
+app.get("/cars",function(req,res){
+    res.render("index");
+});
+
+
+// NEW ROUTE
+app.get("/cars/new",function(req,res){
+    res.render("new");
+});
+
+
+// CREATE ROUTE
+app.post("/cars/new",function(req,res){
+    res.send("create route!");
+});
+
+
+// SHOW ROUTE
+app.get("/cars/:id",function(req,res){
+    res.render("show");
 });
 
 
